050cf3a808257b6b43a183e1a39cc91b36d4eecf
Nirvik Acharya <nirvik640@gmail.com>
2026-05-06 08:24:25 +0545

    MiddleWare add

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth_routes";
 import conversationRoutes from "./routes/conversations";
 import messageRoutes from "./routes/messages";
+import { protect } from "./middleware/auth_middleware";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/conversations", conversationRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/conversations", protect, conversationRoutes);
+app.use("/api/messages", protect, messageRoutes);
 
 export default app;
