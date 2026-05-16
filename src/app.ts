c6f51597abe5dac044211a88e440ffa98b6b8b28
Nirvik Acharya <nirvik640@gmail.com>
2026-05-16 13:57:58 +0545

    app added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import whatsappRoutes from "./routes/whatsapp_routes";
 import authRoutes from "./routes/auth_routes";
 import conversationRoutes from "./routes/conversations";
 import messageRoutes from "./routes/messages";
 import { protect } from "./middleware/auth_middleware";
-import webhookRouter from "./routes/webhook_routes";
 
 const app = express();
 app.use(express.json());
@@ -13,7 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/conversations", protect, conversationRoutes);
 app.use("/api/messages", protect, messageRoutes);
-app.use("/api/webhook", webhookRouter);
-app.use("/api/whatsapp", protect, whatsappRoutes);
 
 export default app;
